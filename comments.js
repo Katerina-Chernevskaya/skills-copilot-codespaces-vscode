1cc4cf0cf695a252f7ff069d602ac0240272c48b
Katerina Chernevskaya <124665014+Katerina-Chernevskaya@users.noreply.github.com>
2023-07-21 17:56:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+// Run: node comments.js
+// Run: node comments.js --port=3000
+// Run: node comments.js --port=3000 --dir=files
+// Run: node comments.js --port=3000 --dir=files --logging
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000 --static
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000 --static --autoindex
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000 --static --autoindex --upload
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000 --static --autoindex --upload --limit=20
+// Run: node comments.js --port=3000 --dir=files --logging --unauthorized --index --cache --delay=2000 --static --autoindex --upload --limit=20 --host=
